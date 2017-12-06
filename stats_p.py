24d913ae2f188d748548fa6844c98c229b7c5740
Sameer Makladeh <[sameermakladeh@gmail.com](mailto:sameermakladeh@gmail.com)>
2017-12-06 21:41:55 +0200

    get from file

--- stats_p.py
+++ stats_p.py
@@ -4,6 +4,18 @@ import math
 #import statistics
 import numpy as np
 #import turtle
+import pandas
+
+# Import data from excel and show it (using pandas module)
+df = pandas.read_excel('data.xlsx')
+col_names = df.columns
+col_data = df.values
+#print (col_names[1])
+#print (col_data[:,1])
+#values = df['Year'].values
+#FORMAT = ['Year','Mean','Min','Max']
+#df_selected = df[FORMAT]
+
 
 def calc_uk(some_list,k):
     # calculates the statistics Mann-Whitney of the list given using indicator
@@ -15,8 +27,8 @@ def calc_uk(some_list,k):
                 count = count + 1
     return count
 
-x= np.random.uniform(0,1)
-print (x)
+#x= np.random.uniform(0,1)
+#print (x)
 
 def calc_mk(some_list):
     # calculates the number MK using statistic Mann-Whitney for each k in the list
@@ -49,7 +61,7 @@ def s1_n(some_list):
         for r in range(1,n_len-j+1): #array starts at [0] so i need to give it the right pointer (n-k is included here)
             two = two + j*(n_len-j-r)
     num = ((n_len+1)/12)*(one + 2*two)
-    s1_n = math.sqrt(num)
+    s1_n = float(math.sqrt(num))
     return s1_n
 
 
@@ -61,25 +73,27 @@ def s2_n(some_list):
             sq = (i*(n_len-i-r))/((n_len-i)*(i+r))
             one = one + math.sqrt(sq)
     num = (n_len-1) + 2*one
-    s2_n = math.sqrt(num)
+    s2_n = float(math.sqrt(num))
     return s2_n
 
 
-list_s = [18.35,19.54,19.5,19.62,21.52,21.48,20.38,21.15,20.29,19.98,19.71,19.81,20.77,20.84,21.09,
-        19.74,21.13,19.43,19.77,19.81,20.81,21.35,18.79,19.43,22.12,21.43,19.73,21.34,20.15,20.79,
-        21.42,20.56,22.23,21.05,22.07,22.31,20.73,21.93,20.73,23.97,21.62,21.67,21.95,23.78,20.87,21.76]
+def main(j):
+    global col_data, col_names
+    for i in range(1,j):
+    #calc for 1 (mk)
+        t_list = col_data[:,i]
+        mk_call = calc_mk(t_list)
+        s1_n_call = s1_n(t_list)
+        print("ratio_1 for ",col_names[i], " = ", mk_call/s1_n_call)
+    #calc for 2 (md)
+        md_call = calc_md(t_list)
+        s2_n_call = s2_n(t_list)
+        print("ratio_2 for ",col_names[i], " = ", md_call/s2_n_call)
 
-mk = calc_mk(list_s)
-s1_n = s1_n(list_s)
-print ("mk stats = ", mk)
-print("stdev_1 = " ,s1_n)
-print("ratio_1 = " ,mk/s1_n)
 
-md = calc_md(list_s)
-s2_n = s2_n(list_s)
-print("md stats = ", md)
-print("stdev_2 = ", s2_n)
-print ("ratio_2 = ", md/s2_n)
+
+main(4)
+
 
 
 '''mu = statistics.mean(list_s)
